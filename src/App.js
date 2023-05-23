4395d97547f715ca7e78b5f43a95ce4d5c8e7fa0
ThienPhuoc21 <thienphuoctran20@gmail.com>
2023-05-23 16:09:24 +0700

    .

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="ReactMovie/" exact="true" element={<Home />} active>
+          <Route path="/ReactMovie/" exact="true" element={<Home />} active>
           </Route>
-          <Route path="ReactMovie/movie" exact="true" element={<Movie />}>
+          <Route path="/ReactMovie/movie" exact="true" element={<Movie />}>
 
           </Route>
-          <Route path="ReactMovie/tv" element={<TV />}>
+          <Route path="/ReactMovie/tv" element={<TV />}>
           </Route>
 
-          <Route path="ReactMovie/film/:movieortv/:id" element={<Film />} >
+          <Route path="/ReactMovie/film/:movieortv/:id" element={<Film />} >
           </Route>
         </Routes>
       </div>
